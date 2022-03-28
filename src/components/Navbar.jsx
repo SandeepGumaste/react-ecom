91662044908da22533380890c64d734b67080006
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2022-03-28 10:21:07 +0530

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,50 @@
 import React from "react";
 import styled from "styled-components";
-
+import { BiSearch } from "react-icons/bi";
 const Container = styled.div`
   height: 60px;
-  background-color: black;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
 `;
 
 const Navbar = () => {
-  return <Container>Navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            input
+            <BiSearch />
+          </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
