3a4d6b82d6737cf9f0640327387f17039748c011
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2022-03-29 01:08:26 +0530

    navbar design complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { BiSearch } from "react-icons/bi";
+import Badge from "@mui/material/Badge";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+
 const Container = styled.div`
   height: 60px;
 `;
@@ -8,10 +10,13 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 const Left = styled.div`
+  display: flex;
   flex: 1;
+  align-items: center;
 `;
 
 const Language = styled.span`
@@ -21,12 +26,35 @@ const Language = styled.span`
 
 const SearchContainer = styled.div`
   border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
 `;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
 const Right = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
   flex: 1;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -36,12 +64,22 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            input
-            <BiSearch />
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>Studio React</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign in</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCartOutlined color="action" />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
