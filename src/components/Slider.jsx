572656d865b3bddf87e142c63e88def91f9067b2
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2022-04-05 22:47:25 +0530

    categories added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -52,13 +52,18 @@ const ImgContainer = styled.div`
   justify-content: end;
 `;
 
-const Img = styled.img`
+const Img = styled.div`
+  width: 100%;
   height: 100%;
+  background-image: url(${(props) => `${props.src}`});
+  background-position: center;
+  background-size: cover;
 `;
 
 const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
+  padding-right: 100px;
 `;
 const Title = styled.h1`
   font-size: 70px;
@@ -72,7 +77,10 @@ const Desc = styled.p`
 const Button = styled.button`
   padding: 10px;
   font-size: 20px;
-  background-color: transparent;
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
 `;
 
 const Slider = () => {
@@ -91,8 +99,8 @@ const Slider = () => {
         <ArrowLeftOutlined />
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
-        {sliderItems.map(({ img, title, desc, bg }) => (
-          <Slide bg={bg}>
+        {sliderItems.map(({ img, title, desc, bg, id }) => (
+          <Slide bg={bg} key={id}>
             <ImgContainer>
               <Img src={img} />
             </ImgContainer>
